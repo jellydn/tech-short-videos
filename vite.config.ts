ec2eaa5d0736f540090bfe11c5805d26fce3bdb8
Dung Duc Huynh (Kaka) <870029+jellydn@users.noreply.github.com>
2024-12-01 11:08:13 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import autoprefixer from "autoprefixer";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+	css: {
+		postcss: {
+			plugins: [autoprefixer],
+		},
+	},
+	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+});
